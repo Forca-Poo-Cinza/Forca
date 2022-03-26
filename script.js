ffa70c4f94f25591509e1feb2c9cff97b48fada1
josuerodrigo.jr80@gmail.com <josuerodrigo.jr80@gmail.com>
2022-03-26 19:45:33 -0300

    Codigo limpo && Css mais estiloso

--- script.js
+++ script.js
@@ -1,126 +1,110 @@
-const dados01 = [];
-const letraadv01 = [];
 let errado = [];
 let segredo = [];
 let palavra = "";
 let chance = 0;
 let derrota = 0;
 let vitoria = 0;
-let img = document.getElementById("trocarimg");
-let alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "Ç"]
+
+let alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "Ç"];
 
 
-class Pessoas {
-    constructor (nome, email){
-        this.nome = nome 
-        this.email = email
+
+let categorias = {
+    Catego: ["Cor", "Fruta", "Animal"],
+    Cor: ["ABOBORA", "AÇAFRAO", "AMARELO", "AMBAR", "AMEIXA", "AMÊNDOA", "AMETISTA", "ANIL", "AZUL", "BEGE", "BORDÔ", "BRANCO", "BRONZE", "CAQUI", "CARAMELO", "CARMESIM", "CARMIM", "CASTANHO", "CEREJA", "CHOCOLATE", "CIANO", "CINZA", "CINZENTO", "COBRE", "CORAL", "CREME", "DAMASCO", "DOURADO", "ESCARLATE", "ESMERALDA", "FERRUGEM", "FÚCSIA", "GELO", "GRENÁ", "GRIS", "ÍNDIGO", "JADE", "JAMBO", "LARANJA", "LAVANDA", "LILÁS", "LIMAO", "LOIRO", "MAGENTA", "MALVA", "MARFIM", "MARROM", "MOSTARDA", "NEGRO", "OCRE", "OLIVA", "OURO","PÊSSEGO", "PRATA", "PRETO", "PÚRPURA", "ROSA", "ROXO", "RUBRO", "SALMÃO", "SÉPIA", "TERRACOTA", "TIJOLO", "TURQUESA", "UVA", "VERDE", "VERMELHO", "VINHO", "VIOLETA"],
+    Fruta: ["ABACATE","ABACAXI","AÇAI","ACEROLA","AMEIXA","CAQUI","CARAMBOLA","CEREJA","DAMASCO","FRAMBOESA","GOIABA","GRAVIOLA","JABOTICABA","LARANJA","LICHIA","MANGA","MEXERICA","MIRTILO","NECTARINA","TAMARINDO"],
+    Animal:["CAMALEAO", "CAPIVARA", "CAMELO", "TARTARUGA", "LEBRE", "GUAXINIM", "GIRAFA", "TUCANO", "OURIÇO", "CISNE", "SAGUI", "ORANGOTANGO", "HIPOPOTAMO", "HIENA", "GUEPARDO", "MOSCA", "MOSQUITO", "CAMARAO", "PACA", "RAPOSA", "BAIACU", "DRAGAO", "MARIPOSA", "UNICORNIO", "NARVAL", "OCAPI", "CEGONHA", "PELICANO", "LACRAIA", "LEMURE", "IGUANA", "CHINCHILA", "CACATUA", "PAVAO", "TARANTULA", "KOALA", "CANGURU", "CALOPSITA", "DINOSSAURO", "TIRANOSSAURO"],
+    sorteio(){  
+        random = Math.floor(Math.random() * categorias.Catego.length);  
+        categoria = categorias.Catego[random];
+
+        random = Math.floor(Math.random() * categorias[categoria].length)
+        
+        palavra = categorias[categoria][random]
+        secreta = new Palavra(categoria, palavra)
     }
 }
 
-function cadastrar() {
-    // entrada:
-    document.getElementById("vitoria").textContent = 0;
-    document.getElementById("derrota").textContent = 0;
-    derrota = 0;
-    vitoria = 0;
+class Palavra {
+    constructor(categoria,palavra){
+        this.categoria = categoria
+        this.palavra = palavra
+    }
+}
+
+class Pessoas {
+    constructor (nome, email, vitoria = 0, derrota = 0){
+        this.nome = nome 
+        this.email = email
+        this.vitoria = vitoria 
+        this.derrota = derrota
+    }   
     
+}
+
+function cadastrar() {
     user = document.getElementById("usuario").value;
     mail = document.getElementById("email").value;
-    console.log(user, mail)
-    cadastrado = new Pessoas(user,mail)
-
-    // saída:
-//    console.table(dados01);
-
-   // limpar a saída
+    cadastro01 = new Pessoas(user,mail)
     usuario.value= '';
-    email.value = '';
-
+    email.value = '';    
+    document.getElementById("vitoria").textContent = cadastro01.vitoria;
+    document.getElementById("derrota").textContent = cadastro01.derrota;
     reset();
 }
 
-function reset() {
+function botaoTodos (a1,a2,a3 = false) {
+    for (let index = 0; index < alfabeto.length; index++) {
+    let a = `btn${alfabeto[index]}`
+    document.getElementById(a).style.backgroundColor = `#${a1}`;
+    document.getElementById(a).style.opacity = `${a2}`;
+    document.getElementById(a).disabled = a3;
+    }
+}
+
+function botaoUnico (b){
+    let a = `btn${b}`;
+        document.getElementById(a).style.backgroundColor = "black";
+        document.getElementById(a).style.opacity = "0.2";
+        document.getElementById(a).disabled = true;
+}
+
+function reset() {    
     document.getElementById("errado").value = "";
     document.getElementById("palavraSecreta").value = ""; 
     document.getElementById("trocarimg").style.visibility = "hidden";
     chance = 0;
     segredo = [];
-    palavra =[];
-    errado=[];
-
-    for (let index = 0; index < alfabeto.length; index++) {
-        let a = `btn${alfabeto[index]}`;
-        document.getElementById(a).style.backgroundColor = "#612726";
-        document.getElementById(a).style.opacity = "1";
-        document.getElementById(a).disabled = false;
-    }
-
-    let categoria = ["Cor", "Animal", "Fruta"];
-
-    let teste = (Math.random()*((categoria.length - 1))).toFixed(0);
-    let dica = categoria[teste]
-    document.getElementById("categoria").value = dica;
-
-    if (dica == "Cor"){
-        let cores = ["ABOBORA", "AÇAFRAO", "AMARELO", "AMBAR", "AMEIXA", "AMÊNDOA", "AMETISTA", "ANIL", "AZUL", "BEGE", "BORDÔ", "BRANCO", "BRONZE", "CAQUI", "CARAMELO", "CARMESIM", "CARMIM", "CASTANHO", "CEREJA", "CHOCOLATE", "CIANO", "CINZA", "CINZENTO", "COBRE", "CORAL", "CREME", "DAMASCO", "DOURADO", "ESCARLATE", "ESMERALDA", "FERRUGEM", "FÚCSIA", "GELO", "GRENÁ", "GRIS", "ÍNDIGO", "JADE", "JAMBO", "LARANJA", "LAVANDA", "LILÁS", "LIMAO", "LOIRO", "MAGENTA", "MALVA", "MARFIM", "MARROM", "MOSTARDA", "NEGRO", "OCRE", "OLIVA", "OURO","PÊSSEGO", "PRATA", "PRETO", "PÚRPURA", "ROSA", "ROXO", "RUBRO", "SALMÃO", "SÉPIA", "TERRACOTA", "TIJOLO", "TURQUESA", "UVA", "VERDE", "VERMELHO", "VINHO", "VIOLETA"];
-        let teste1 = (Math.random()*((cores.length - 1))).toFixed(0);
-        palavra = cores[teste1]
-        console.log(palavra)
-
-    } else if (dica == "Fruta"){
-        let frutas = ["ABACATE","ABACAXI","AÇAI","ACEROLA","AMEIXA","CAQUI","CARAMBOLA","CEREJA","DAMASCO","FRAMBOESA","GOIABA","GRAVIOLA","JABOTICABA","LARANJA","LICHIA","MANGA","MEXERICA","MIRTILO","NECTARINA","TAMARINDO"]
-        let teste1 = (Math.random()*((frutas.length - 1))).toFixed(0);
-        palavra = frutas[teste1]
-        console.log(palavra)
-
-    } else {
-        let animal = ["CAMALEAO", "CAPIVARA", "CAMELO", "TARTARUGA", "LEBRE", "GUAXINIM", "GIRAFA", "TUCANO", "OURIÇO", "CISNE", "SAGUI", "ORANGOTANGO", "HIPOPOTAMO", "HIENA", "GUEPARDO", "MOSCA", "MOSQUITO", "CAMARAO", "PACA", "RAPOSA", "BAIACU", "DRAGAO", "MARIPOSA", "UNICORNIO", "NARVAL", "OCAPI", "CEGONHA", "PELICANO", "LACRAIA", "LEMURE", "IGUANA", "CHINCHILA", "CACATUA", "PAVAO", "TARANTULA", "KOALA", "CANGURU", "CALOPSITA", "DINOSSAURO", "TIRANOSSAURO"];
-        let teste1 = (Math.random()*((animal.length - 1))).toFixed(0);
-        palavra = animal[teste1]
-        console.log(palavra)
-
-    }
+    errado=[];    
     
-    console.log(palavra.length)
+    botaoTodos(746262, 1, false)
+    categorias.sorteio()
+    
+    document.getElementById("categoria").value = secreta.categoria;
 
     for (let i = 1; i <= palavra.length; i++){
         segredo.push("____")
     }
-    console.log(segredo.length)
-    document.getElementById("secreto").innerHTML = segredo.join("  ");
-    
+    document.getElementById("secreto").innerHTML = segredo.join("  ");    
 };
 
+function letra01(letra1){   
 
-
-function letra01(letra1){
-    console.log(letra1);
     let acerto = false;    
 
     for (let j = 0; j < palavra.length; j++){
 
         if(palavra[j] == letra1){
             acerto = true;
-            console.log(j);
             segredo[j] = letra1;
-            console.log(segredo.join(" "))
             document.getElementById("secreto").innerHTML = segredo.join(" ");
             comparar = segredo.find(element => element == "____");
-            console.log(comparar)
-            let a = `btn${letra1}`;
-            document.getElementById(a).style.backgroundColor = "black";
-            document.getElementById(a).style.opacity = "0.2";
-            document.getElementById(a).disabled = true;
+            botaoUnico(letra1)
+
             if (comparar == undefined){
                 vitoria = vitoria + 1;
                 document.getElementById("vitoria").textContent = vitoria;
-                console.log(vitoria)
-                for (let index = 0; index < alfabeto.length; index++) {
-                    let a = `btn${alfabeto[index]}`;
-                    document.getElementById(a).style.backgroundColor = "black";
-                    document.getElementById(a).style.opacity = "0.2";
-                    document.getElementById(a).disabled = true;
-                }                
+                botaoTodos(808080, 0.2, true)
             }
         }
     }
@@ -129,39 +113,25 @@ function letra01(letra1){
         errado.push(letra1)
         document.getElementById("errado").value = errado;
         chance += 1;
-        console.log(chance) 
-        let a = `btn${letra1}`;
-        document.getElementById(a).style.backgroundColor = "black";
-        document.getElementById(a).style.opacity = "0.2";
-        document.getElementById(a).disabled = true;
-
+        botaoUnico(letra1);
     }
+
     if (chance != 0) {
         document.getElementById("trocarimg").style.visibility = "visible"
     }
 
-    if (chance <= 6 && chance >= 1){
+    if (chance >= 1 && chance <= 6){
         document.getElementById("trocarimg").src=`Imagens/0${chance}.png`;
-        } else if (chance == 7) {
-            derrota = derrota + 1;
-            document.getElementById("derrota").textContent = derrota;
-            console.log(palavra)
-            document.getElementById("trocarimg").src=`Imagens/07.png`;
-            document.getElementById("palavraSecreta").value = palavra; 
-            
-            for (let index = 0; index < alfabeto.length; index++) {
-                let a = `btn${alfabeto[index]}`;
-                document.getElementById(a).style.backgroundColor = "black";
-                document.getElementById(a).style.opacity = "0.2";
-                document.getElementById(a).disabled = true;
-            }
-            
-        }      
-        
-    //garante que num fique alternando entre 1 e 2
-
-    console.log(chance)
+    } else if (chance == 7) {
+        derrota = derrota + 1;
+        document.getElementById("derrota").textContent = derrota;
+        document.getElementById("trocarimg").src=`Imagens/07.png`;
+        document.getElementById("palavraSecreta").value = palavra; 
+        botaoTodos(808080, 0.2, true)            
+        }
 }
+
+
 
 
 
